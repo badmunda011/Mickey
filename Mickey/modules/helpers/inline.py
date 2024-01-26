f35272ce59133c5f6c1c4cc65e35faff8292fb93
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-01-26 14:03:01 +0530

    Update inline.py

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -6,10 +6,20 @@ from Mickey import MickeyBot
 
 DEV_OP = [
     [
+        InlineKeyboardButton(text="🥀 ᴏᴡɴᴇʀ 🥀", user_id=OWNER),
+        InlineKeyboardButton(text="✨ ꜱᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/ll_THE_BAD_BOT_ll"),
+    ],
+    [
         InlineKeyboardButton(
             text="🧸 ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ 🧸",
             url=f"https://t.me/{MickeyBot.username}?startgroup=true",
         ),
+    ],
+    [
+        InlineKeyboardButton(text="🚀 ʜᴇʟᴘ & ᴄᴍᴅs 🚀", callback_data="HELP"),
+    ],
+    [
+        InlineKeyboardButton(text="☁️ ᴀʙᴏᴜᴛ ☁️", callback_data="ABOUT"),
     ],
 ]
 
